d2cfb131a7d127952c0f0380c4d526880baba913
ao <1147608791@qq.com>
2019-09-04 10:53:36 +0800

    Initial Commit

---
+++ datas/list-data.js
@@ -0,0 +1,155 @@
+let list_data = [
+  
+  {
+
+    date: '7 20 2019',
+    title: '哥谭梦魇',
+    detail_img: '/images/detail/carousel/02.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_brief:'Batman是最不漫画的漫画英雄，也正因为这一点，使之最有可能成为跨越犯罪剧情片和漫画英雄片界限的特例。而这一次，编导精准的抓到了这一点，把这部电影打造得丰富严谨又酷劲十足，无论是满足视听还是人性复杂层面的挖掘上都做到了堪称完美！',
+    detail_content: '从亲眼目睹父母被人杀死的阴影中走出来的“蝙蝠侠”，经历了成长之后，已经不再是那个桀骜不的孤单英雄了。在警官吉姆·戈登和检查官哈维·登特的通力帮助下，“蝙蝠侠”无后顾之忧地继续满世界的奔波，与日益增长起来的犯罪威胁做着永无休止的争斗，而他所在的高谭市，也是进展最为明显的地方，犯罪率以一种惊人的速度持续下降着，毕竟对方是能够上天入地的“蝙蝠侠”，不借两个胆子谁还敢造次呢？不过像高谭这种科技与污秽并存的城市，平静是不可能维持太久的，果不其然，新一轮的混乱很快就席卷了整个城市，人们再一被被恐慌所笼罩着，而声称愿意为这一切负责的，自然就是所有混乱的源头以及支配者--“小丑”了。\n&emsp;&emsp;先不管“小丑”掀起一个又一个犯罪的狂潮的最终目的为何，他的企图都是邪恶的，所作所为更是早就危害到了高谭市民的正常生活……其中自然包括了“蝙蝠侠”身边几个非常重要的人，而他需要做的，就是将这股新的危机全部亲自用手捏得粉碎。然而在面对着这个有史以来最具针对性、最恶毒的对手时，“蝙蝠侠”却不得不从他的地下军械库里搬出每一件能够用得上的高科技武器，还得时刻纠结着为他曾经信仰的一切寻找答案。',
+    headImgSrc: '/images/detail/carousel/02.jpg',
+    author: '哥谭一哥',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 999,
+    attention_count: 666,
+    detail: '钢铁战队。。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 0
+  },
+  {
+    
+    date: '6 28 2019',
+    title: '深海之王',
+    detail_img: '/images/detail/carousel/01.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_brief:'也别问DC粉儿了，就问你们刚从影院出来的，咱海王硬不硬？！咱海王猛不猛？！温子仁还拍下一集要不要？！！',
+    detail_content: '许多年前，亚特兰蒂斯女王（妮可·基德曼 Nicole Kidman 饰）和人类相知相恋，共同孕育了爱情的结晶——后来被陆地人称为海王的亚瑟·库瑞（杰森·莫玛 Jason Momoa 饰）。在成长的过程中，亚瑟接受海底导师维科（威廉·达福 Willem Dafoe 饰）的严苛训练，时刻渴望去看望母亲，然而作为混血的私生子这却是奢望。与此同时，亚瑟的同母异父兄弟奥姆（帕特里克·威尔森 Patrick Wilson 饰）成为亚特兰蒂斯的国王，他不满陆地人类对大海的荼毒与污染，遂谋划联合其他海底王国发动对陆地的全面战争。为了阻止他的野心，维科和奥姆的未婚妻湄拉（艾梅柏·希尔德 Amber Heard 饰）将亚瑟带到海底世界。\n&emsp;&emsp;宿命推动着亚瑟，去寻找失落已久的三叉戟，建立一个更加开明的海底王国……',
+    headImgSrc: '/images/detail/carousel/01.jpg',
+    author: '海王',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: 'DC海洋一哥',
+    music: {
+      dataUrl: 'http://www.ytmp3.cn/down/50395.mp3', // 音乐链接
+      title: '一路向北',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 1
+  },
+  {
+
+    date: '7 19 2019',
+    title: '神奇女侠',
+    detail_img: '/images/detail/carousel/03.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_brief:'非常惊喜，这位名不见经传的女导演给所有男性导演上了一课：如何在保证场面和视效的前提下，兼具大爱和小爱。感情戏拿捏的恰到好处，最后的大战效果也不错，旁边的妹子一直在抽泣，这次终于为DC扳回一城。不过最大的缺憾在于，最后反派的扮相真的丑爆了，瞬间出戏…以及，评论里说圣母的都什么思想…',
+    detail_content: '戴安娜（盖尔·加朵 Gal Gadot 饰）是女王希波吕忒（康妮·尼尔森 Connie Nielsen 饰）的女儿，自幼生活在天堂岛上。巨大的屏障将这座岛屿同外界的纷纷扰扰隔开犹如一个世外桃源，而岛上生活着的亦都是女性。在女武官安提奥普（罗宾·莱特 Robin Wright 饰）的教导之下，戴安娜习得了高强的武艺，而她的体内，似乎隐藏着某种强大的未知力量。\n&emsp;&emsp;一场意外中，一位名为史蒂夫（克里斯·派恩 Chris Pine 饰）的男子来到了岛上，从他口中，戴安娜得知外面的世界正在经历战争的磨难，而造成这一切的罪魁祸首，是战神阿瑞斯（大卫·休里斯 David Thewlis 饰）。为了拯救人类于水火之中，戴安娜依然拿起了长剑与盾牌，发誓要彻底摧毁阿瑞斯的阴谋。',
+    headImgSrc: '/images/detail/carousel/03.jpg',
+    author: '女侠',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: 'DC一姐',
+    music: {
+      dataUrl: 'http://www.ytmp3.cn/down/50355.mp3', // 音乐链接
+      title: '听海',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 2
+  },
+  {
+    date: '7 12 2019',
+    title: '大超',
+    detail_img: '/images/detail/list/j2.jpg',
+    detail_brief:'我打他他不死，使劲锤，助跑一公里锤，还是不死，抱起来砸，往墙上砸，往地上砸，砸碎十条街，砸碎八十栋大楼，就是砸不动，我怎么没想起来直接把他掐死啊！',
+    detail_content: '遥远的宇宙深处，随着氪星的毁灭，超人的传奇故事拉开序幕。氪星人乔·艾尔（罗素·克劳 Russell Crowe 饰）抵死反抗佐德将军（迈克尔·珊农 Michael Shannon 饰）的邪恶计划，冒险将刚出生不久的儿子卡尔·艾尔被送到银河系另一边的地球。卡尔降落在美国堪萨斯一座小镇，他幸运地成为乔纳森·肯特（凯文·科斯特纳 Kevin Costner 饰）和玛莎（黛安·莲恩 Diane Lane 饰）的养子，并且得名克拉克。克拉克从小显露出与众不同的能力与特征，但这些带给他惊讶的同时还有数不清的烦恼。通过养父的引导，克拉克（亨利·卡维尔 Henry Cavill 饰）慢慢弄清自己的身世以及所肩负的使命。终于有一天，遥远的故乡“同胞”莅临。\n&emsp;&emsp;站在故乡与地球、邪恶与正义的两端，超人即将面临重大的抉择……',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 999,
+    headImgSrc: '/images/detail/list/j2.jpg',
+    author: '克拉克',
+    attention_count: 726,
+    avatar: '/images/avatar/1.png',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46101.mp3', // 音乐链接
+      title: 'Sunset Jesus-Avicii',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 3
+  },
+  {
+    date: '2018/3/17 下午3:30:35',
+    title: '贾静雯作品展',
+    detail_img: '/images/detail/list/j3.jpg',
+    detail_content: '2001年，贾静雯将工作重心转向内地，并主演了传奇剧《大汉天子》。2002年，她凭借武侠剧《倚天屠龙记》在内地赢得更高关注度[2]  。2003年，贾静雯获得FHM全球百大性感美女亚洲区冠军。2006年，他主演了现代剧《悲伤时唱首歌》。此后几年，贾静雯相继出演了传奇剧《太平公主秘史》、剧情片《不能说的夏天》等影视作品',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    headImgSrc: '/images/detail/list/j3.jpg',
+    author: '新华社',
+    avatar: '/images/avatar/2.png',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46102.mp3', // 音乐链接
+      title: '汪峰 - 儿时',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 4
+  },
+  {
+
+    date: 'sep 19 2016',
+    title: '娱乐新闻',
+    detail_img: '/images/detail/list/j4.jpg',
+    avatar: '/images/avatar/3.png',
+    detail_content: '2014年，贾静雯与小自己九岁的台湾演员修杰楷相恋[41]  。2015年5月5日，贾静雯的男友修杰楷在微博宣布了贾静雯已身怀有孕的消息，并发出了他与贾静雯和梧桐妹的两张合照，随后，贾静雯转发了此条微博并希望得到大家的祝福[42]  ；同年8月7日，贾静雯以剖宫产方式生下了与修杰楷的第一个女儿“咘咘”，而此前两人已正式注册结婚[43]  。',
+    headImgSrc: '/images/detail/list/j4.jpg',
+    author: '李白3',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '女神。。。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46110.mp3', // 音乐链接
+      title: '曲婉婷 - 我的歌声里-(电视剧《在线爱》主题曲)',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 5
+  },
+  {
+
+    date: 'sep 19 2016',
+    title: '社会活动',
+    detail_img: '/images/detail/list/j6.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_content: '2012年，贾静雯担任公益大使，发起为弱势儿童和青少年课后照护计划，并为此次活动献唱了个人单曲《许一个愿望》。2014年，贾静雯参加了最终梦想年度时尚魅力女性颁奖盛典，并在典礼上获得了亲情天使奖[50] ',
+    headImgSrc: '/images/detail/list/j6.jpg',
+    author: '新华社',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '女神2。。。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 6
+  },
+];
+
+module.exports = {list_data};
