dc9caf38321cd9f339a099e6d47a9a3f38d53e10
ao <1147608791@qq.com>
2019-09-05 11:16:20 +0800

    数据

--- datas/list-data.js
+++ datas/list-data.js
@@ -4,8 +4,8 @@ let list_data = [
 
     date: '7 20 2019',
     title: '哥谭梦魇',
-    detail_img: '/images/detail/carousel/02.jpg',
-    avatar: '/images/avatar/4.png',
+    detail_img: '/images/detail/carousel/01.jpg',
+    avatar: '/images/avatar/0.png',
     detail_brief:'Batman是最不漫画的漫画英雄，也正因为这一点，使之最有可能成为跨越犯罪剧情片和漫画英雄片界限的特例。而这一次，编导精准的抓到了这一点，把这部电影打造得丰富严谨又酷劲十足，无论是满足视听还是人性复杂层面的挖掘上都做到了堪称完美！',
     detail_content: '从亲眼目睹父母被人杀死的阴影中走出来的“蝙蝠侠”，经历了成长之后，已经不再是那个桀骜不的孤单英雄了。在警官吉姆·戈登和检查官哈维·登特的通力帮助下，“蝙蝠侠”无后顾之忧地继续满世界的奔波，与日益增长起来的犯罪威胁做着永无休止的争斗，而他所在的高谭市，也是进展最为明显的地方，犯罪率以一种惊人的速度持续下降着，毕竟对方是能够上天入地的“蝙蝠侠”，不借两个胆子谁还敢造次呢？不过像高谭这种科技与污秽并存的城市，平静是不可能维持太久的，果不其然，新一轮的混乱很快就席卷了整个城市，人们再一被被恐慌所笼罩着，而声称愿意为这一切负责的，自然就是所有混乱的源头以及支配者--“小丑”了。\n&emsp;&emsp;先不管“小丑”掀起一个又一个犯罪的狂潮的最终目的为何，他的企图都是邪恶的，所作所为更是早就危害到了高谭市民的正常生活……其中自然包括了“蝙蝠侠”身边几个非常重要的人，而他需要做的，就是将这股新的危机全部亲自用手捏得粉碎。然而在面对着这个有史以来最具针对性、最恶毒的对手时，“蝙蝠侠”却不得不从他的地下军械库里搬出每一件能够用得上的高科技武器，还得时刻纠结着为他曾经信仰的一切寻找答案。',
     headImgSrc: '/images/detail/carousel/02.jpg',
@@ -28,7 +28,7 @@ let list_data = [
     date: '6 28 2019',
     title: '深海之王',
     detail_img: '/images/detail/carousel/01.jpg',
-    avatar: '/images/avatar/4.png',
+    avatar: '/images/avatar/1.png',
     detail_brief:'也别问DC粉儿了，就问你们刚从影院出来的，咱海王硬不硬？！咱海王猛不猛？！温子仁还拍下一集要不要？！！',
     detail_content: '许多年前，亚特兰蒂斯女王（妮可·基德曼 Nicole Kidman 饰）和人类相知相恋，共同孕育了爱情的结晶——后来被陆地人称为海王的亚瑟·库瑞（杰森·莫玛 Jason Momoa 饰）。在成长的过程中，亚瑟接受海底导师维科（威廉·达福 Willem Dafoe 饰）的严苛训练，时刻渴望去看望母亲，然而作为混血的私生子这却是奢望。与此同时，亚瑟的同母异父兄弟奥姆（帕特里克·威尔森 Patrick Wilson 饰）成为亚特兰蒂斯的国王，他不满陆地人类对大海的荼毒与污染，遂谋划联合其他海底王国发动对陆地的全面战争。为了阻止他的野心，维科和奥姆的未婚妻湄拉（艾梅柏·希尔德 Amber Heard 饰）将亚瑟带到海底世界。\n&emsp;&emsp;宿命推动着亚瑟，去寻找失落已久的三叉戟，建立一个更加开明的海底王国……',
     headImgSrc: '/images/detail/carousel/01.jpg',
@@ -51,7 +51,7 @@ let list_data = [
     date: '7 19 2019',
     title: '神奇女侠',
     detail_img: '/images/detail/carousel/03.jpg',
-    avatar: '/images/avatar/4.png',
+    avatar: '/images/avatar/2.png',
     detail_brief:'非常惊喜，这位名不见经传的女导演给所有男性导演上了一课：如何在保证场面和视效的前提下，兼具大爱和小爱。感情戏拿捏的恰到好处，最后的大战效果也不错，旁边的妹子一直在抽泣，这次终于为DC扳回一城。不过最大的缺憾在于，最后反派的扮相真的丑爆了，瞬间出戏…以及，评论里说圣母的都什么思想…',
     detail_content: '戴安娜（盖尔·加朵 Gal Gadot 饰）是女王希波吕忒（康妮·尼尔森 Connie Nielsen 饰）的女儿，自幼生活在天堂岛上。巨大的屏障将这座岛屿同外界的纷纷扰扰隔开犹如一个世外桃源，而岛上生活着的亦都是女性。在女武官安提奥普（罗宾·莱特 Robin Wright 饰）的教导之下，戴安娜习得了高强的武艺，而她的体内，似乎隐藏着某种强大的未知力量。\n&emsp;&emsp;一场意外中，一位名为史蒂夫（克里斯·派恩 Chris Pine 饰）的男子来到了岛上，从他口中，戴安娜得知外面的世界正在经历战争的磨难，而造成这一切的罪魁祸首，是战神阿瑞斯（大卫·休里斯 David Thewlis 饰）。为了拯救人类于水火之中，戴安娜依然拿起了长剑与盾牌，发誓要彻底摧毁阿瑞斯的阴谋。',
     headImgSrc: '/images/detail/carousel/03.jpg',
@@ -81,7 +81,7 @@ let list_data = [
     headImgSrc: '/images/detail/list/j2.jpg',
     author: '克拉克',
     attention_count: 726,
-    avatar: '/images/avatar/1.png',
+    avatar: '/images/avatar/3.png',
     music: {
       dataUrl: 'http://up.mcyt.net/down/46101.mp3', // 音乐链接
       title: 'Sunset Jesus-Avicii',   // 音乐标题
@@ -101,7 +101,7 @@ let list_data = [
     attention_count: 672,
     headImgSrc: '/images/detail/list/j3.jpg',
     author: 'Shazam',
-    avatar: '/images/avatar/2.png',
+    avatar: '/images/avatar/4.png',
     music: {
       dataUrl: 'http://up.mcyt.net/down/46102.mp3', // 音乐链接
       title: '汪峰 - 儿时',   // 音乐标题
@@ -113,7 +113,7 @@ let list_data = [
     date: '8 19 2019',
     title: '最快的男人',
     detail_img: '/images/detail/list/j4.jpg',
-    avatar: '/images/avatar/3.png',
+    avatar: '/images/avatar/5.png',
     detail_brief: '妈呀，男主帅的掉裤子，我说这么眼熟！原来是Glee的483小鲜肉！！妈呀帅哭了！不过对CW已经不抱什么希望了，浓缩到10集的话应该会好看很多！毕竟这种东西一部电影就讲完了。。。。看完首集，竟然觉得还挺好看的呢！哈哈哈哈！',
     detail_content: '11岁时，艾伦（格兰特·古斯汀 Grant Gustin 饰）的母亲离奇死亡，而他的父亲则被当成了凶手。之后，警探乔（杰西·马丁 Jesse L. Martin 饰）收留了孤苦伶仃的艾伦，在乔的耳濡目染之下，成年后的艾伦成为了一名CSI鉴证分析员，在此过程中，他一直没有放弃过对于母亲真正死因的调查。\n&emsp;&emsp;艾伦十分迷恋科学家哈里森（汤姆·加瓦那 Tom Cavanagh 饰）所研究的粒子加速器，然而，一场意外让加速器发生了爆炸，艾伦被击中陷入了昏迷。九个月后，终于苏醒的艾伦惊讶的发现自己体内产生了神奇的变化，因此而获得了特殊能力。随着时间的推移，艾伦开始明白，这个世界上，还有很多和自己一样拥有超能力的人存在，而保护市民的安全成为了他新的责任和任务',
     headImgSrc: '/images/detail/list/j4.jpg',
@@ -134,7 +134,7 @@ let list_data = [
     date: '7 19 2019',
     title: '钢铁的意志',
     detail_img: '/images/detail/list/j4.jpg',
-    avatar: '/images/avatar/3.png',
+    avatar: '/images/avatar/6.png',
     detail_brief: '在日前华纳公布的拍摄计划中，“钢骨”赫然在列，将拥有属于自己的独立电影。费舍表示在这之前他并不知道DC打算给他的角色一部独立电影。',
     detail_content: '本名维克多·“维克”·史东（Victor“Vic”Stone），原本是运动员，为高中美式足球队的明星外接手，拥有相当优秀的体能因此维克希望大学时能够继续玩球，但因和父亲两人关系较疏远而一直没有机会。在其父缺席几次维克的比赛后，维克终于前往父亲的STAR实验室表达不满，父亲赛拉斯（Silas）告诉他的儿子地球正受到来自各地的超人类威胁著，而研究这些相关外星科技的实验比维克的足球还要重要，维克多气馁地正打算离开时，STAR实验室中的一个管线意外地爆开，使他和其他数名科学家受到了重伤甚至死亡。赛拉斯并不打算要让儿子受到和妻子相同的悲剧，因此在T.O. Morrow和Sarah Charles两位科学家的协助下，维克在美国政府收集各种外星科技的实验室-红屋中成功保住了性命，在抢救过程中因为基于天启星科技制作的机器的影响，维克吸收了各种红屋的高科技物品，最后成为了半人半机械的生化人。\n&emsp;&emsp;因达克赛德（Darkseid）入侵的缘故，使得后来组成正义联盟的初期成员们选择合作御敌，拥有高科技身躯的维克因此和超人等人作战，并在知道父亲的用心后原谅了他。后来“钢骨”成为了备受众人爱戴的英雄。',
     headImgSrc: '/images/detail/list/j4.jpg',
